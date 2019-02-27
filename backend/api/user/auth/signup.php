65b62451272d11d078c53998f8f96266346a45ba
Himel <himel35-1389@diu.edu.bd>
2019-02-27 23:08:01 +0600

    Signup and Sign  in Model and api fix!

--- backend/api/user/auth/signup.php
+++ backend/api/user/auth/signup.php
@@ -14,6 +14,11 @@ $db = $database->connect();
 
 $user = new User($db);
 
+if((empty($_POST['firstName'])) || (empty($_POST['lastName'])) || (empty($_POST['email'])) || (empty($_POST['birthday'])) || (empty($_POST['occupation'])) || (empty($_POST['gender'])) || (empty($_POST['password']))){
+    echo('{"msg": "You must fill every require field!","status": "error"}');
+    die();
+}
+
 $user->firstName = $_POST['firstName'];
 $user->lastName = $_POST['lastName'];
 //$user->userName = $_POST['userName'];
@@ -24,20 +29,13 @@ $user->gender = $_POST['gender'];
 $user->password = $_POST['password'];
 
 if($user->signup()){
-    $userArray = array(
-        "status" => true,
-        "message" => "successfully signup",
-        "id" => $user->id ,
-        "userName" => $user->email 
-    );
+    
+    echo('{"msg": "Signup Successfull!","status": "ok"}');
 }else{
-    $userArray = array(
-        "status" => false,
-        "message" => "email already exists!"
-    );
+    echo('{"msg": "User already exists!","status": "error"}');
 }
 
-print_r(json_encode($userArray));
+// print_r(json_encode($userArray));
 
 
 
