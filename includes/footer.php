16ff27e0d72fe1de1d1a719dafae8ae4075e2e36
jubel ahmed <jubel35-1648@diu.edu.bd>
2019-02-28 05:59:07 +0600

    footer updated

--- includes/footer.php
+++ includes/footer.php
@@ -1,10 +1,9 @@
 <footer class="footer">
     <div class="container">
-        <p class="text-muted text-center"> Copyright &copy; Information hub-2019 - All rights reserved </p>
+        <p class="text-muted text-center"> Copyright &copy; Information hub 2019 - All rights reserved </p>
     </div>
 </footer>
 
 </body>
-
 
 </html>
