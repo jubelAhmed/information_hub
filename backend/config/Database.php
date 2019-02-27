f08c7523c850bd6dc9e54f8bc7c5437cc6d1388e
Himel <himel35-1389@diu.edu.bd>
2019-02-27 19:31:35 +0600

    Simple Changes

--- backend/config/Database.php
+++ backend/config/Database.php
@@ -13,7 +13,6 @@ class Database
     public function connect()
     {
         $this->conn = null;
-      
 
         try {
             $this->conn = new PDO("mysql:host={$this->host};dbname={$this->db_name}", $this->user_name, $this->password);
