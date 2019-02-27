812f419ea4d8ba9d204e8da10346bd3704497b2a
jubel ahmed <jubel35-1648@diu.edu.bd>
2019-02-28 02:41:32 +0600

    job apply updated

--- backend/api/user/job/apply_job.php
+++ backend/api/user/job/apply_job.php
@@ -15,14 +15,36 @@ $db = $database->connect();
 $applyJob = new ApplyJob($db);
 
 // 
-$applicatId= 28; // get from seassion
+$applicatId= 37; // get from seassion
 $jobId = 2; // get from user clikable link
+
+// add apply job
 
 if($applyJob->applyjob($jobId,$applicatId)){
     echo('{
-        "msg": "job apply success",
+        "msg": "job apply successful",
         "status": "ok"
       }');
+}else{
+    echo('{
+        "msg": "job apply faild or already applied job",
+        "status": "failed"
+      }');
 }
+
+// get apply job list by applicat id
+/* 
+    $applyJobList = $applyJob->getAllApplyJob($applicatId);
+
+    if($applyJobList->rowCount()>0){
+        while($row = $applyJobList->fetch(PDO::FETCH_ASSOC)){
+            extract($row);
+            echo $job_id;
+            echo "\n";
+            echo $applicant_id;
+        }
+    } */
+
+
 
 ?>
