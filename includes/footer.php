69e4bc27ea52d7a9eba7f607e3410627bf1398dd
jubel ahmed <jubel35-1648@diu.edu.bd>
2019-02-28 07:24:46 +0600

    dynamic job skill add

--- includes/footer.php
+++ includes/footer.php
@@ -1,7 +1,9 @@
 <footer class="footer">
-  <div class="container">
-    <p class="text-muted text-center"> Copyright  &copy; Information hub 2019 - All rights reserved </p>
-  </div>
+    <div class="container">
+        <p class="text-muted text-center"> Copyright &copy; Information hub 2019 - All rights reserved </p>
+    </div>
 </footer>
+
 </body>
+
 </html>
