8843fb35c0fe65a9508449f0ad790a54e72d4196
Himel <himel35-1389@diu.edu.bd>
2019-04-07 00:41:06 +0600

    Search Filter Added

---
+++ profile/jobs/filter.php
@@ -0,0 +1,22 @@
+<html>
+	<title>Filter Job</title>
+	<head>
+		<script src="https://code.jquery.com/jquery-3.3.1.js"></script>
+		<script src="../../ajax/job-post/search-job.js"></script>
+	</head>
+
+	<body>
+		
+		<input type="text" id="whatIwantToSearch" placeholder="C++">
+		<input type="button" id="search" value="Search">
+		<div id="infoID">
+			
+			<H3>Job Title</H3>
+			<P>Company</P>
+			<p>Website</p>
+			<p>C++, Java, python</p>
+			<hr>
+		</div>
+
+	</body>
+</html>
